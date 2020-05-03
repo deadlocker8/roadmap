4fd04cac05fcfce401e29b061ba766e470417223
Robert Goldmann <deadlocker@gmx.de>
2020-05-03 10:10:24 +0200

    api: use TheCodeLabs util packages

--- api/RoadmapAPIServer.py
+++ api/RoadmapAPIServer.py
@@ -1,77 +1,62 @@
 import datetime
-import json
+import logging
+import os
 
-from flask import Flask, send_from_directory, request
+from TheCodeLabs_BaseUtils import DefaultLogger
+from TheCodeLabs_FlaskUtils import FlaskBaseApp
 from flask import jsonify
+from flask import send_from_directory, request
 from flask_jwt_extended import (
     JWTManager, create_access_token
 )
-from gevent.pywsgi import WSGIServer
 
+import Constants
 from Database import Database
 from RequestValidator import RequestValidator, ValidationError
 from UserService import UserService
 from blueprints import SubTaskAPI, MilestoneAPI, TaskAPI, RoadmapAPI
 
-with open('version.json', 'r') as f:
-    VERSION = json.load(f)
-VERSION = VERSION['version']
-
-with open("settings.json", "r") as f:
-    SETTINGS = json.load(f)
-SERVER_SETTINGS = SETTINGS["server"]
-
-app = Flask(__name__)
-
-app.config["JWT_SECRET_KEY"] = SERVER_SETTINGS["secret"]
-jwt = JWTManager(app)
-
-database = Database(SETTINGS["database"])
-userService = UserService(SETTINGS["users"])
+LOGGER = DefaultLogger().create_logger_if_not_exists(Constants.APP_NAME)
 
 
-@app.route('/')
-def index():
-    return send_from_directory("docs", "api.html")
+class RoadmapApi(FlaskBaseApp):
+    def __init__(self, appName: str, rootDir: str, logger: logging.Logger, settingsPath: str):
+        super().__init__(appName, rootDir, logger, settingsPath=settingsPath)
+        self._database = Database(self._settings["database"])
+        self._userService = UserService(self._settings["users"])
 
+    def _register_blueprints(self, app):
+        app.config["JWT_SECRET_KEY"] = self._serverSettings["secret"]
+        jwt = JWTManager(app)
 
-@app.route('/version', methods=['GET'])
-def version():
-    return jsonify(VERSION)
+        @app.route('/')
+        def index():
+            return send_from_directory("docs", "api.html")
 
+        @app.route('/login', methods=['POST'])
+        def login():
+            try:
+                parameters = RequestValidator.validate(request, ["username", "password"])
+            except ValidationError as e:
+                return e.response, 400
 
-@app.route('/login', methods=['POST'])
-def login():
-    try:
-        parameters = RequestValidator.validate(request, ["username", "password"])
-    except ValidationError as e:
-        return e.response, 400
+            password = self._userService.get_password_by_username(parameters["username"])
+            if password is None:
+                return jsonify({"success": False, "msg": "Unknown username"}), 401
 
-    password = userService.get_password_by_username(parameters["username"])
-    if password is None:
-        return jsonify({"success": False, "msg": "Unknown username"}), 401
+            if password != parameters["password"]:
+                return jsonify({"success": False, "msg": "Bad credentials"}), 401
 
-    if password != parameters["password"]:
-        return jsonify({"success": False, "msg": "Bad credentials"}), 401
+            expires = datetime.timedelta(hours=1)
+            access_token = create_access_token(identity=parameters["username"], expires_delta=expires)
+            return jsonify(access_token=access_token), 200
 
-    expires = datetime.timedelta(hours=1)
-    access_token = create_access_token(identity=parameters["username"], expires_delta=expires)
-    return jsonify(access_token=access_token), 200
-
-
-app.register_blueprint(RoadmapAPI.construct_blueprint(database))
-app.register_blueprint(MilestoneAPI.construct_blueprint(database))
-app.register_blueprint(TaskAPI.construct_blueprint(database))
-app.register_blueprint(SubTaskAPI.construct_blueprint(database))
+        app.register_blueprint(RoadmapAPI.construct_blueprint(self._database))
+        app.register_blueprint(MilestoneAPI.construct_blueprint(self._database))
+        app.register_blueprint(TaskAPI.construct_blueprint(self._database))
+        app.register_blueprint(SubTaskAPI.construct_blueprint(self._database))
 
 
 if __name__ == "__main__":
-    if SERVER_SETTINGS["useSSL"]:
-        http_server = WSGIServer((SERVER_SETTINGS["listen"], SERVER_SETTINGS["port"]), app,
-                                 keyfile=SERVER_SETTINGS["keyfile"], certfile=SERVER_SETTINGS["certfile"])
-    else:
-        http_server = WSGIServer((SERVER_SETTINGS["listen"], SERVER_SETTINGS["port"]), app)
-
-    print('RoadmapClient {}({}) - Listening on {}:{}...'.format(VERSION['name'], VERSION['code'],
-                                                                SERVER_SETTINGS['listen'], SERVER_SETTINGS['port']))
-    http_server.serve_forever()
+    roadmapApi = RoadmapApi(Constants.APP_NAME, os.path.dirname(__file__), LOGGER, 'settings.json')
+    roadmapApi.start_server()
